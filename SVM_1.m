35ff31da6afb91f29c3c5a6823b63de3129b02b7
tyskin K <kongqijin@outlook.com>
2017-08-08 13:14:11 +0100

    SVM
    不如libsvm好用

---
+++ SVM_1.m
@@ -0,0 +1,19 @@
+%svm_1
+
+
+
+
+% load MNIST_all_single.mat 
+% X=tr_x_scale;
+% Y=tr_y;
+% 
+% SVMModel = fitcsvm(X,Y,'Standardize',true,'KernelFunction','gaussian',...
+%     'KernelScale','auto');
+
+
+load MNIST_all_single.mat 
+X=tr_x_scale;
+Y=tr_y;
+t = templateSVM('KernelFunction','gaussian');
+Mdl = fitcecoc(X,Y,...
+    'Learners',t);
